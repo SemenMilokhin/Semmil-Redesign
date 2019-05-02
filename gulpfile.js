da87c2fe8001f78890fa5e7e18200dcb2a08d6b5
Semen Milokhin <semen.milokhin@yandex.ru>
2019-05-02 17:37:44 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var browserSync  = require('browser-sync').create(),
+	autoprefixer = require('gulp-autoprefixer'),
+	gulp         = require('gulp'),
+	sass         = require('gulp-sass');
+
+function initBrowserSyncAndWatch() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		}
+	});
+	gulp.watch('app/sass/**/*.+(scss|sass)').on('change', compileSass);
+	gulp.watch(['app/js/**/*.js','app/**/*.html','app/**/*.php']).on('change', browserSync.reload);
+};
+
+function compileSass() {
+	return gulp.src('app/sass/**/*.scss')
+	.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+	.pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream());
+};
+
+exports.watch = gulp.series(compileSass, initBrowserSyncAndWatch);
+exports.default = gulp.series(compileSass, initBrowserSyncAndWatch);
